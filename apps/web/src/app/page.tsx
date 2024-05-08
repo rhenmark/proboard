ad597cb426bb63f08704105eb758d058bad62e34
Rhen Mark <rhenmarkc@gmail.com>
2024-05-08 09:35:14 +0800

    chore: fix fetch policy

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,8 +3,8 @@ import client from '../utils/apollo-client';
 import { GET_PROJECTS_LIST } from '../query/home';
 import HeroBanner from '../components/page-banner/PageBanner';
 import ProjectsSection from '../components/project-section/ProjectSection';
-import DeveloperSection from '../components/developer-section/DeveloperSection'
-import DownloadBanner from '../components/download-banner'
+import DeveloperSection from '../components/developer-section/DeveloperSection';
+import DownloadBanner from '../components/download-banner';
 
 export default async function Index() {
   const data = await getData();
@@ -20,11 +20,13 @@ export default async function Index() {
 }
 
 async function getData() {
-  const { data, loading } = await client.query({ query: GET_PROJECTS_LIST });
+  const { data, loading, error } = await client.query({
+    query: GET_PROJECTS_LIST,
+    fetchPolicy: 'network-only',
+  });
   return {
     loading,
     projects: data?.proboardCollection?.items || [],
     developers: data?.developerCollection?.items || [],
   };
 }
-
