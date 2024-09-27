2f9feb836ff5034d487b21d0b4440f21054898bf
rhenmark <rhenmarkc@gmail.com>
2024-09-27 23:07:13 +0800

    chore: fix env

--- apps/payndit/src/app/login/page.tsx
+++ apps/payndit/src/app/login/page.tsx
@@ -38,6 +38,7 @@ function Login() {
     console.log(data);
     handleSignin();
   });
+  
   return (
     <div className="bg-black text-white">
       <div className="max-w-sm bg-black/90 mx-auto min-h-dvh grid pt-20 p-4">
